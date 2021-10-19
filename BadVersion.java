f693e5553291bcda1cc536795dadd14c02d1fc40
Viraj Patil <69743653+virajPatil11@users.noreply.github.com>
2021-10-19 20:25:57 +0530

    Add files via upload

---
+++ BadVersion.java
@@ -0,0 +1,20 @@
+package Again;
+
+public class BadVersion {
+	public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(isBadVersion(mid)){
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
